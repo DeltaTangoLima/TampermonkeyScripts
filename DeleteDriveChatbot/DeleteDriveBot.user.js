2f1a95a497ad73765886110a4078e67599dffd78
Dan Lehman <dan@leh.mn>
2024-08-08 07:58:49 +1000

    Updated for, hopefully, better reliability of chat window removal

--- DeleteDriveChatbot/DeleteDriveBot.user.js
+++ DeleteDriveChatbot/DeleteDriveBot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Delete Drive.com.au crap
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  Hides annoying elements on the drive.com.au website
 // @author       DeltaTangoLima
 // @match        *://*.drive.com.au/*
@@ -9,7 +9,7 @@
 // @updateURL    https://github.com/DeltaTangoLima/TampermonkeyScripts/raw/main/DeleteDriveChatbot/DeleteDriveBot.user.js
 // @icon         https://raw.githubusercontent.com/DeltaTangoLima/TampermonkeyScripts/main/DeleteDriveChatbot/no-bots.jpg
 // @grant        GM_addStyle
-// ==/UserScript==
+// @run-at       document-idle
 
 const remove = (sel) => document.querySelectorAll(sel).forEach(el => el.remove());
 
@@ -17,3 +17,12 @@ remove('[class*="liveChat_drive-container__"]');
 remove('[class*="layout_drive-layout__ad-bg__"]');
 remove('[class*="layout_drive-layout__ad-bg--mobile-persist-sticky__"]');
 remove('[class*="roofrack-wrapper"]');
+
+(new MutationObserver(check)).observe(document, {childList: true, subtree: true});
+
+function check(changes, observer) {
+    if(document.querySelector('[class*="liveChat_drive-container__"]')) {
+        observer.disconnect();
+        remove('[class*="liveChat_drive-container__"]');
+    }
+}
